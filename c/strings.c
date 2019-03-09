adcc5a14d060ad19bc19d589778d859ea75b5b0b
Jason_000 <jasonhall96686@yahoo.com>
2019-03-09 02:34:18 +0000

    Implement .find()

--- c/strings.c
+++ c/strings.c
@@ -56,12 +56,57 @@ static bool containsString(int argCount) {
     return true;
 }
 
+bool static findString(int argCount) {
+    if (argCount < 2 || argCount > 3) {
+        runtimeError("find() takes either 2 or 3 arguments (%d  given)", argCount);
+        return false;
+    }
+
+    int index = 1;
+
+    if (argCount == 3) {
+        if (!IS_NUMBER(peek(0))) {
+            runtimeError("Index passed to find() must be a number");
+            return false;
+        }
+
+        index = AS_NUMBER(pop());
+    }
+
+    if (!IS_STRING(peek(0))) {
+        runtimeError("Substring passed to find() must be a string");
+        return false;
+    }
+
+    char *substr = AS_CSTRING(pop());
+    char *string = AS_CSTRING(pop());
+
+    int position = 0;
+
+    for (int i = 0; i < index; ++i) {
+        char *result = strstr(string, substr);
+        if (!result) {
+            position = -1;
+            break;
+        }
+
+        position += (result - string) + (i * strlen(substr));
+        string = result + strlen(substr);
+    }
+
+    push(NUMBER_VAL(position));
+    return true;
+}
+
 bool stringMethods(char *method, int argCount) {
     if (strcmp(method, "split") == 0) {
         return splitString(argCount);
     } else if (strcmp(method, "contains") == 0) {
         return containsString(argCount);
+    } else if (strcmp(method, "find") == 0) {
+        return findString(argCount);
     }
+
 
     runtimeError("String has no method %s()", method);
     return false;
