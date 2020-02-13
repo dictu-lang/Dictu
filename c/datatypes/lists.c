6b2f2264095639ed9b0128b75590e9e36f20ab7e
Jason Hall <jasonhall96686@yahoo.com>
2020-02-13 12:56:12 +0000

    Free more memory and use correct index

--- c/datatypes/lists.c
+++ c/datatypes/lists.c
@@ -191,9 +191,10 @@ static bool joinListItem(int argCount) {
     fullString[index] = '\0';
     push(OBJ_VAL(copyString(fullString, index)));
 
-    if (!IS_STRING(list->values.values[j])) {
+    if (!IS_STRING(list->values.values[list->values.count - 1])) {
         free(output);
     }
+    free(fullString);
 
     return true;
 }
