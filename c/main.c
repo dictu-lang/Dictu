3b1664306d64c78c1637b63b3e8972e3c06c55e8
Jason Hall <jasonhall96686@yahoo.com>
2020-02-18 01:16:16 +0000

    Bump version to 0.3.2

--- c/main.c
+++ c/main.c
@@ -10,7 +10,7 @@
 
 #include "linenoise.h"
 
-#define VERSION "Dictu Version: 0.3.1\n"
+#define VERSION "Dictu Version: 0.3.2\n"
 
 static bool replCountBraces(char *line) {
     int leftBraces = 0;
