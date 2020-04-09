2a1bf8fc8352bd91648ae1bd3f19e7896cb0a093
aglai <aga.chatzimanikas@gmail.com>
2020-04-09 09:52:12 +0300

    Add a toString() method to NumberType.

--- c/datatypes/number.c
+++ c/datatypes/number.c
@@ -1,6 +1,19 @@
 #include "number.h"
 #include "../vm.h"
 
+static Value toStringNumber(VM *vm, int argCount, Value *args) {
+    if (argCount != 0) {
+        runtimeError(vm, "toString() takes no arguments (%d given)", argCount);
+        return EMPTY_VAL;
+    }
+
+    double number = AS_NUMBER(args[0]);
+    int numberStringLength = snprintf(NULL, 0, "%.15g", number) + 1;
+    char numberString[numberStringLength];
+    snprintf(numberString, numberStringLength, "%.15g", number);
+    return OBJ_VAL(copyString(vm, numberString, numberStringLength - 1));
+}
+
 void declareNumberMethods(VM *vm) {
-    // defineNative(vm, &vm->numberMethods, "", func);
+    defineNative(vm, &vm->numberMethods, "toString", toStringNumber);
 }
