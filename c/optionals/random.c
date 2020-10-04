96207c0b32d02134590e905a876dd1b2a6aad3ba
Sean McLoughlin <smcloughlin1015@gmail.com>
2020-10-03 20:58:52 -0700

    Implement Random.select()

--- c/optionals/random.c
+++ c/optionals/random.c
@@ -1,7 +1,7 @@
 #include "random.h"
 
 static Value randomRandom(VM *vm, int argCount, Value *args) {
-    args = args;  // FIXME: Can't get -Werror to surpress not using args
+    UNUSED(args);
     if (argCount > 0) {
         runtimeError(vm, "random() takes 0 arguments (%d given)", argCount);
         return EMPTY_VAL;
@@ -26,9 +26,32 @@ static Value randomRange(VM *vm, int argCount, Value *args) {
     return NUMBER_VAL(random_val);
 }
 
-// static Value randomSelect(VM *vm, int argCount, Value *args) {
+static Value randomSelect(VM *vm, int argCount, Value *args) {
+    if (argCount == 0) {
+        runtimeError(vm, "select() takes one argument (%0d provided)", argCount);
+        return EMPTY_VAL;
+    }
 
-// }
+    if (!IS_LIST(args[0])) {
+        runtimeError(vm, "select() argument must be a list");
+        return EMPTY_VAL;        
+    }
+
+    ObjList *list = AS_LIST(args[0]);
+    argCount = list->values.count;
+    args = list->values.values;
+
+    for (int i = 0; i < argCount; ++i) {
+        Value value = args[i];
+        if (!IS_NUMBER(value)) {
+            runtimeError(vm, "A non-number value passed to select()");
+            return EMPTY_VAL;
+        }
+    }
+
+    int index = rand() % argCount;
+    return args[index];
+}
 
 ObjModule *createRandomClass(VM *vm) {
     ObjString *name = copyString(vm, "Random", 6);
@@ -41,7 +64,7 @@ ObjModule *createRandomClass(VM *vm) {
      */
     defineNative(vm, &module->values, "random", randomRandom);
     defineNative(vm, &module->values, "range", randomRange);
-    // defineNative(vm, &module->values, "random", randomSelect);
+    defineNative(vm, &module->values, "select", randomSelect);
 
     pop(vm);
     pop(vm);
