418212e61f870c9b96d0b6aeced8ee098e48e1d3
Sean McLoughlin <smcloughlin1015@gmail.com>
2020-10-03 20:46:00 -0700

    Implement Random.range()

--- c/optionals/random.c
+++ c/optionals/random.c
@@ -1,6 +1,6 @@
 #include "random.h"
 
-static Value randomRandom(VM * vm, int argCount, Value *args) {
+static Value randomRandom(VM *vm, int argCount, Value *args) {
     args = args;  // FIXME: Can't get -Werror to surpress not using args
     if (argCount > 0) {
         runtimeError(vm, "random() takes 0 arguments (%d given)", argCount);
@@ -9,6 +9,26 @@ static Value randomRandom(VM * vm, int argCount, Value *args) {
     return NUMBER_VAL(rand() % 2);
 }
 
+static Value randomRange(VM *vm, int argCount, Value *args) {
+    if (argCount != 2) {
+        runtimeError(vm, "range() takes 2 arguments (%0d given)", argCount);
+        return EMPTY_VAL;
+    }
+
+    if (!IS_NUMBER(args[0]) || !IS_NUMBER(args[1])) {
+        runtimeError(vm, "range() arguments must be numbers");
+        return EMPTY_VAL;
+    }    
+
+    int upper = AS_NUMBER(args[1]);
+    int lower = AS_NUMBER(args[0]);
+    int random_val = (rand() % (upper - lower + 1)) + lower;
+    return NUMBER_VAL(random_val);
+}
+
+// static Value randomSelect(VM *vm, int argCount, Value *args) {
+
+// }
 
 ObjModule *createRandomClass(VM *vm) {
     ObjString *name = copyString(vm, "Random", 6);
@@ -20,7 +40,8 @@ ObjModule *createRandomClass(VM *vm) {
      * Define Random methods
      */
     defineNative(vm, &module->values, "random", randomRandom);
-    // TODO: Define randomRange and randomSelect
+    defineNative(vm, &module->values, "range", randomRange);
+    // defineNative(vm, &module->values, "random", randomSelect);
 
     pop(vm);
     pop(vm);
