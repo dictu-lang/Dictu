627ba9803d9affe2929cd6c0406f662e51b4026f
Kayomn <kayomn@kayomn.net>
2020-11-14 20:49:40 +0000

    Fix indentation in Registry implementation file

--- c/registry.c
+++ c/registry.c
@@ -3,73 +3,73 @@
 #include "memory.h"
 
 static bool isHandleValid(const Registry *registry, const uint32_t handle) {
-	return ((handle < registry->count) && registry->entries[handle].isOccupied);
+    return ((handle < registry->count) && registry->entries[handle].isOccupied);
 }
 
 void initRegistry(Registry *registry) {
-	registry->entries = NULL;
-	registry->count = 0;
-	registry->capacity = 0;
-	registry->nextFree = 0;
+    registry->entries = NULL;
+    registry->count = 0;
+    registry->capacity = 0;
+    registry->nextFree = 0;
 }
 
 void freeRegistry(VM *vm, Registry *registry) {
-	FREE_ARRAY(vm, RegistryEntry, registry->entries, registry->capacity + 1);
+    FREE_ARRAY(vm, RegistryEntry, registry->entries, registry->capacity + 1);
 }
 
 uint32_t registryInsert(VM *vm, Registry *registry, Value value) {
-	uint32_t handle;
+    uint32_t handle;
 
-	if (registry->nextFree == registry->capacity) {
-		// Conditionally grow if the existing registry buffer is full.
-		if (registry->count == registry->capacity) {
-			uint32_t const oldCapacity = registry->capacity;
-			registry->capacity *= 2;
+    if (registry->nextFree == registry->capacity) {
+        // Conditionally grow if the existing registry buffer is full.
+        if (registry->count == registry->capacity) {
+            uint32_t const oldCapacity = registry->capacity;
+            registry->capacity *= 2;
 
-			registry->entries = GROW_ARRAY(
-				vm,
-				registry->entries,
-				RegistryEntry,
-				oldCapacity,
-				registry->capacity
-			);
-		}
+            registry->entries = GROW_ARRAY(
+                vm,
+                registry->entries,
+                RegistryEntry,
+                oldCapacity,
+                registry->capacity
+            );
+        }
 
-		handle = registry->count;
-		registry->nextFree += 1;
-	} else {
-		handle = registry->nextFree;
-		registry->nextFree = registry->entries[handle].vacant;
-	}
+        handle = registry->count;
+        registry->nextFree += 1;
+    } else {
+        handle = registry->nextFree;
+        registry->nextFree = registry->entries[handle].vacant;
+    }
 
-	registry->entries[handle] = (RegistryEntry){
-		.occupied = value,
-		.isOccupied = true
-	};
+    registry->entries[handle] = (RegistryEntry){
+        .occupied = value,
+        .isOccupied = true
+    };
 
-	registry->count += 1;
+    registry->count += 1;
 
-	return handle;
+    return handle;
 }
 
 Value registryLookup(Registry *registry, const uint32_t handle) {
-	return (isHandleValid(registry, handle) ? registry->entries[handle].occupied : NIL_VAL);
+    return (isHandleValid(registry, handle) ? registry->entries[handle].occupied : NIL_VAL);
 }
 
 Value registryRemove(Registry *registry, const uint32_t handle) {
-	if (isHandleValid(registry, handle)) {
-		Value value = registry->entries[handle].occupied;
+    if (isHandleValid(registry, handle)) {
+        Value value = registry->entries[handle].occupied;
 
-		registry->entries[handle] = (RegistryEntry){
-			.vacant = registry->nextFree,
-			.isOccupied = false
-		};
+        registry->entries[handle] = (RegistryEntry){
+            .vacant = registry->nextFree,
+            .isOccupied = false
+        };
 
-		registry->nextFree = handle;
-		registry->count -= 1;
+        registry->nextFree = handle;
+        registry->count -= 1;
 
-		return value;
-	}
+        return value;
+    }
 
-	return NIL_VAL;
+    return NIL_VAL;
 }
