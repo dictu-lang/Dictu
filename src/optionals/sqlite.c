8996ec6bed5d487c98960857b803985120258937
Jason_000 <jasonhall96686@gmail.com>
2020-12-26 23:19:06 +0000

    Enable FKs by default for SQLite

--- src/optionals/sqlite.c
+++ src/optionals/sqlite.c
@@ -55,7 +55,7 @@ static Value execute(DictuVM *vm, int argCount, Value *args) {
     Database *db = AS_SQLITE_DATABASE(args[0]);
     char *sql = AS_CSTRING(args[1]);
     ObjList *list = NULL;
-    int parameterCount = countParameters(sql);;
+    int parameterCount = countParameters(sql);
     int argumentCount = 0;
 
     if (argCount == 2) {
@@ -105,6 +105,7 @@ static Value execute(DictuVM *vm, int argCount, Value *args) {
 
             sqlite3_finalize(result.stmt);
             char *error = (char *)sqlite3_errmsg(db->db);
+            pop(vm);
             return newResultError(vm, error);
         }
 
@@ -185,6 +186,16 @@ static Value connectSqlite(DictuVM *vm, int argCount, Value *args) {
         char *error = (char *)sqlite3_errmsg(db->db);
         return newResultError(vm, error);
     }
+
+    sqlite3_stmt *res;
+    err = sqlite3_prepare_v2(db->db, "PRAGMA foreign_keys = ON;", -1, &res, 0);
+
+    if (err) {
+        char *error = (char *)sqlite3_errmsg(db->db);
+        return newResultError(vm, error);
+    }
+
+    sqlite3_finalize(res);
 
     return newResultSuccess(vm, OBJ_VAL(abstract));
 }
