76f77c5c626ded05cfa8430904165d708eb44ca6
Jason_000 <jasonhall96686@gmail.com>
2021-11-02 19:00:21 +0000

    Correct the name for the guard

--- src/optionals/unittest/unittest.h
+++ src/optionals/unittest/unittest.h
@@ -1,8 +1,8 @@
-#ifndef DICTU_API_FUNCTOOLS_H
-#define DICTU_API_FUNCTOOLS_H
+#ifndef DICTU_API_UNITTEST_H
+#define DICTU_API_UNITTEST_H
 
 #include "../optionals.h"
 
 Value createUnitTestModule(DictuVM *vm);
 
-#endif //DICTU_API_FUNCTOOLS_H
+#endif //DICTU_API_UNITTEST_H
