a3f0bf826716198a7cff51060d3bab5da1ecdded
Jason_000 <jasonhall96686@gmail.com>
2021-12-26 19:13:04 +0000

    Define file descriptor constants on windows

--- src/optionals/log.h
+++ src/optionals/log.h
@@ -3,6 +3,12 @@
 
 #include "../vm/vm.h"
 
+#ifdef _WIN32
+#define	STDIN_FILENO	0	/* standard input file descriptor */
+#define	STDOUT_FILENO	1	/* standard output file descriptor */
+#define	STDERR_FILENO	2	/* standard error file descriptor */
+#endif
+
 Value createLogModule(DictuVM *vm);
 
 #endif //_dictu_log_h
