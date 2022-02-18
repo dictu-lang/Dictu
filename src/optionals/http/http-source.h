9c8bc0bea262ccb2a4926e4153627a943bac21e1
Jason_000 <jasonhall96686@gmail.com>
2022-02-18 20:02:24 +0000

    Move JSON import to function call

--- src/optionals/http/http-source.h
+++ src/optionals/http/http-source.h
@@ -1,10 +1,10 @@
-#define DICTU_HTTP_SOURCE "import JSON;\n" \
-"\n" \
-"class Response {\n" \
+#define DICTU_HTTP_SOURCE "class Response {\n" \
 "    // content, headers and statusCode set via C\n" \
 "    init() {}\n" \
 "\n" \
 "    json() {\n" \
+"        import JSON;\n" \
+"\n" \
 "        return JSON.parse(this.content);\n" \
 "    }\n" \
 "}\n" \
