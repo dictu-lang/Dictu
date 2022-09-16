4f4bf147352d65ad3e4c3b7c0dbaabc54d86a538
Jason_000 <jasonhall96686@gmail.com>
2022-09-16 21:16:43 +0100

    Use the correct unpack operator

--- src/optionals/object/object-source.h
+++ src/optionals/object/object-source.h
@@ -1,8 +1,8 @@
-#define DICTU_OBJECT_SOURCE "def createFrom(className, ...arguments) {\n" \
-"    import Object;\n" \
+#define DICTU_OBJECT_SOURCE "import Object;\n" \
 "\n" \
+"def createFrom(className, ...arguments) {\n" \
 "    return Object.__getClassRef(className).matchWrap(\n" \
-"        def (klass) => klass(),\n" \
+"        def (klass) => klass(...arguments),\n" \
 "        def (error) => error\n" \
 "    );\n" \
 "}\n" \
