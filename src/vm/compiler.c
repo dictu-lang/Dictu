1209b9fdf247a6ac219dcf79a0ece2a056d0803c
Amuthan Mannar <amuthanmannar4@gmail.com>
2022-11-05 15:17:01 +0530

    Fixed list literal issue

--- src/vm/compiler.c
+++ src/vm/compiler.c
@@ -61,6 +61,12 @@ static void advance(Parser *parser) {
     }
 }
 
+static void recede(Parser *parser){
+    for (int i = 0; i < parser->current.length; ++i) {
+            backTrack(&parser->scanner);
+    }
+}
+
 static void consume(Compiler *compiler, TokenType type, const char *message) {
     if (compiler->parser->current.type == type) {
         advance(compiler->parser);
@@ -1921,7 +1927,7 @@ static void varDeclaration(Compiler *compiler, bool constant) {
     consume(compiler, TOKEN_SEMICOLON, "Expect ';' after variable declaration.");
 }
 
-static void expressionStatement(Compiler *compiler) {
+static void expressionStatement(Compiler *compiler) { 
     Token previous = compiler->parser->previous;
     advance(compiler->parser);
     TokenType t = compiler->parser->current.type;
@@ -2484,13 +2490,40 @@ static void whileStatement(Compiler *compiler) {
 static void unpackListStatement(Compiler *compiler){
     int varCount = 0;
     Token variables[255];
+    Token previous=compiler->parser->previous;
     do {
+        if(!check(compiler,TOKEN_IDENTIFIER)){
+            if(varCount>0){
+                recede(compiler->parser);
+            }
+            int varsAndCommas=varCount+(varCount-1);
+            for(int var=0;var<varsAndCommas;var++){
+                recede(compiler->parser);
+            }
+            recede(compiler->parser);
+            compiler->parser->current=previous;
+            expressionStatement(compiler);
+            return;
+        }
         consume(compiler, TOKEN_IDENTIFIER, "Expect variable name.");
         variables[varCount] = compiler->parser->previous;
         varCount++;
     } while (match(compiler, TOKEN_COMMA));
 
     consume(compiler, TOKEN_RIGHT_BRACKET, "Expect ']' after list destructure.");
+
+    if(!check(compiler, TOKEN_EQUAL)){
+        recede(compiler->parser);
+        int varsAndCommas=varCount+(varCount-1);
+        for(int var=0;var<varsAndCommas;var++){
+                recede(compiler->parser);
+        }
+        recede(compiler->parser);
+        compiler->parser->current=previous;
+        expressionStatement(compiler);
+        return;
+    }
+
     consume(compiler, TOKEN_EQUAL, "Expect '=' after list destructure.");
 
     expression(compiler);
