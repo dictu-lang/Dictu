075b29d07fb08c6b8ae0b57b64ea35ce5dec25cc
Brian Downs <brian.downs@gmail.com>
2022-12-13 19:44:21 -0700

    make all the things work
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- src/optionals/argparse/argparse-source.h
+++ src/optionals/argparse/argparse-source.h
@@ -30,13 +30,18 @@
 "\n" \
 "    private addFlag(flagType, flag, desc, required, ...metavar) {\n" \
 "        if (not this.flagExists(flag)) {\n" \
-"            this.preArgs.push({\n" \
+"            var arg = {\n" \
 "                'type': flagType,\n" \
 "                'flag': flag,\n" \
 "                'desc': desc,\n" \
-"                'required': required,\n" \
-"                'metavr': metavar\n" \
-"            });\n" \
+"                'required': required\n" \
+"            };\n" \
+"\n" \
+"            if (metavar.len() == 1) {\n" \
+"                arg['metavar'] = metavar[0];\n" \
+"            }\n" \
+"\n" \
+"            this.preArgs.push(arg);\n" \
 "\n" \
 "            if (required) {\n" \
 "                this.required.push(flag);\n" \
@@ -63,6 +68,7 @@
 "    usage() {\n" \
 "        if (this.userUsage == '') {\n" \
 "            var u = 'usage: {}\n    {}\n\n'.format(this.name, this.desc);\n" \
+"\n" \
 "            for (var i = 0; i < this.preArgs.len(); i+=1) {\n" \
 "                u += '    {}    {}\n'.format(this.preArgs[i]['flag'], this.preArgs[i]['desc']);\n" \
 "            }\n" \
@@ -95,29 +101,48 @@
 "            for (var j = 0; j < this.preArgs.len(); j+=1) {\n" \
 "                if (System.argv[i] == this.preArgs[j]['flag']) {\n" \
 "                    if (this.preArgs[j]['type'] == 'bool') {\n" \
-"                        this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), true);\n" \
+"                        if (this.preArgs[j].exists('metavar') and this.preArgs[j]['metavar'] != '') {\n" \
+"                            this.args.setAttribute(this.preArgs[j]['metavar'], true);\n" \
+"                        } else {\n" \
+"                            this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), true);\n" \
+"                        }\n" \
+"                        \n" \
 "                    } else if (this.preArgs[j]['type'] == 'list') {\n" \
 "                        if (i == (System.argv.len() - 1) or System.argv[i+1][0] == '-') {\n" \
-"                            return Error('{} requires an argument');\n" \
+"                            return Error('{} requires an argument'.format(System.argv[i]));\n" \
 "                        }\n" \
 "\n" \
-"                        this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), System.argv[i+1].split(','));\n" \
+"                        if (this.preArgs[j].exists('metavar') and this.preArgs[j]['metavar'] != '') {\n" \
+"                            this.args.setAttribute(this.preArgs[j]['metavar'], System.argv[i+1].split(','));   \n" \
+"                        } else {\n" \
+"                            this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), System.argv[i+1].split(','));\n" \
+"                        }\n" \
 "                    } else if (this.preArgs[j]['type'] == 'number') {\n" \
 "                        if (i == (System.argv.len() - 1) or System.argv[i+1][0] == '-') {\n" \
-"                            return Error('{} requires an argument');\n" \
+"                            return Error('{} requires an argument'.format(System.argv[i]));\n" \
 "                        }\n" \
+"\n" \
 "                        const res = System.argv[i+1].toNumber();\n" \
 "                        if (not res.success()) {\n" \
 "                            return Error('{} arg must be a number'.format(System.argv[i]));\n" \
 "                        }\n" \
 "\n" \
-"                        this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), res.unwrap());\n" \
+"                        if (this.preArgs[j].exists('metavar') and this.preArgs[j]['metavar'] != '') {\n" \
+"                            print(this.preArgs[j]['metavar']);\n" \
+"                            this.args.setAttribute(this.preArgs[j]['metavar'], res.unwrap());\n" \
+"                        } else {\n" \
+"                            this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), res.unwrap());\n" \
+"                        }\n" \
 "                    } else {\n" \
 "                        if (i == (System.argv.len() - 1) or System.argv[i+1][0] == '-') {\n" \
-"                            return Error('{} requires an argument');\n" \
+"                            return Error('{} requires an argument'.format(System.argv[i]));\n" \
 "                        }\n" \
 "\n" \
-"                        this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), System.argv[i+1]);\n" \
+"                        if (this.preArgs[j].exists('metavar') and this.preArgs[j]['metavar'] != '') {\n" \
+"                            this.args.setAttribute(this.preArgs[j]['metavar'], System.argv[i+1]);\n" \
+"                        } else {\n" \
+"                            this.args.setAttribute(this.preArgs[j]['flag'].replace('-', ''), System.argv[i+1]);\n" \
+"                        }\n" \
 "                    }\n" \
 "                }\n" \
 "            }\n" \
