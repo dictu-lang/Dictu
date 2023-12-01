6687a21e5ce22518e1e09627f876bb067471d583
Jason_000 <jasonhall96686@gmail.com>
2023-12-01 13:10:32 +0000

    Fix an issue with internal table type

--- src/vm/table.c
+++ src/vm/table.c
@@ -10,22 +10,22 @@
 
 void initTable(Table *table) {
     table->count = 0;
-    table->capacityMask = -1;
+    table->capacityMask = 0;
     table->entries = NULL;
 }
 
 void freeTable(DictuVM *vm, Table *table) {
-    FREE_ARRAY(vm, Entry, table->entries, table->capacityMask + 1);
+    FREE_ARRAY(vm, Entry, table->entries, table->capacityMask);
     initTable(table);
 }
 
 static Entry *findEntry(Entry *entries, int capacityMask,
                         ObjString *key) {
-    uint32_t index = key->hash & capacityMask;
-    Entry *tombstone = NULL;
+    uint32_t index = key->hash & (capacityMask - 1);
+    Entry* tombstone = NULL;
 
     for (;;) {
-        Entry *entry = &entries[index];
+        Entry* entry = &entries[index];
 
         if (entry->key == NULL) {
             if (IS_NIL(entry->value)) {
@@ -40,7 +40,7 @@ static Entry *findEntry(Entry *entries, int capacityMask,
             return entry;
         }
 
-        index = (index + 1) & capacityMask;
+        index = (index + 1) & (capacityMask - 1);
     }
 }
 
@@ -55,34 +55,33 @@ bool tableGet(Table *table, ObjString *key, Value *value) {
 }
 
 static void adjustCapacity(DictuVM *vm, Table *table, int capacityMask) {
-    Entry *entries = ALLOCATE(vm, Entry, capacityMask + 1);
-    for (int i = 0; i <= capacityMask; i++) {
+    Entry* entries = ALLOCATE(vm, Entry, capacityMask);
+    for (int i = 0; i < capacityMask; i++) {
         entries[i].key = NULL;
         entries[i].value = NIL_VAL;
     }
 
     table->count = 0;
 
-    for (int i = 0; i <= table->capacityMask; i++) {
-        Entry *entry = &table->entries[i];
+    for (int i = 0; i < table->capacityMask; i++) {
+        Entry* entry = &table->entries[i];
         if (entry->key == NULL) continue;
 
-        Entry *dest = findEntry(entries, capacityMask, entry->key);
+        Entry* dest = findEntry(entries, capacityMask, entry->key);
         dest->key = entry->key;
         dest->value = entry->value;
         table->count++;
     }
 
-    FREE_ARRAY(vm, Entry, table->entries, table->capacityMask + 1);
+    FREE_ARRAY(vm, Entry, table->entries, table->capacityMask);
     table->entries = entries;
     table->capacityMask = capacityMask;
 }
 
 bool tableSet(DictuVM *vm, Table *table, ObjString *key, Value value) {
-    if (table->count + 1 > (table->capacityMask + 1) * TABLE_MAX_LOAD) {
-        // Figure out the new table size.
-        int capacityMask = GROW_CAPACITY(table->capacityMask + 1) - 1;
-        adjustCapacity(vm, table, capacityMask);
+    if (table->count + 1 > table->capacityMask * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacityMask);
+        adjustCapacity(vm, table, capacity);
     }
 
     Entry *entry = findEntry(table->entries, table->capacityMask, key);
@@ -99,20 +98,19 @@ bool tableDelete(DictuVM *vm, Table *table, ObjString *key) {
     UNUSED(vm);
     if (table->count == 0) return false;
 
-    Entry *entry = findEntry(table->entries, table->capacityMask, key);
+    // Find the entry.
+    Entry* entry = findEntry(table->entries, table->capacityMask, key);
     if (entry->key == NULL) return false;
 
     // Place a tombstone in the entry.
-    table->count--;
     entry->key = NULL;
     entry->value = BOOL_VAL(true);
-
     return true;
 }
 
 void tableAddAll(DictuVM *vm, Table *from, Table *to) {
-    for (int i = 0; i <= from->capacityMask; i++) {
-        Entry *entry = &from->entries[i];
+    for (int i = 0; i < from->capacityMask; i++) {
+        Entry* entry = &from->entries[i];
         if (entry->key != NULL) {
             tableSet(vm, to, entry->key, entry->value);
         }
@@ -127,13 +125,12 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
 
     // Figure out where to insert it in the table. Use open addressing and
     // basic linear probing.
-
-    uint32_t index = hash & table->capacityMask;
+    uint32_t index = hash & (table->capacityMask - 1);
 
     for (;;) {
-        Entry *entry = &table->entries[index];
-
+        Entry* entry = &table->entries[index];
         if (entry->key == NULL) {
+            // Stop if we find an empty non-tombstone entry.
             if (IS_NIL(entry->value)) return NULL;
         } else if (entry->key->length == length &&
                    entry->key->hash == hash &&
@@ -142,14 +139,13 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
             return entry->key;
         }
 
-        // Try the next slot.
-        index = (index + 1) & table->capacityMask;
+        index = (index + 1) & (table->capacityMask - 1);
     }
 }
 
-void tableRemoveWhite(DictuVM *vm, Table *table) {
-    for (int i = 0; i <= table->capacityMask; i++) {
-        Entry *entry = &table->entries[i];
+void tableRemoveWhite(DictuVM *vm, Table* table) {
+    for (int i = 0; i < table->capacityMask; i++) {
+        Entry* entry = &table->entries[i];
         if (entry->key != NULL && !entry->key->obj.isDark) {
             tableDelete(vm, table, entry->key);
         }
@@ -157,7 +153,7 @@ void tableRemoveWhite(DictuVM *vm, Table *table) {
 }
 
 void grayTable(DictuVM *vm, Table *table) {
-    for (int i = 0; i <= table->capacityMask; i++) {
+    for (int i = 0; i < table->capacityMask; i++) {
         Entry *entry = &table->entries[i];
         grayObject(vm, (Obj *) entry->key);
         grayValue(vm, entry->value);
