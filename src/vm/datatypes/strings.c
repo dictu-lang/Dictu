2e0b7725b36c25fbe085a0dc7b6fda86cb91a07f
Brian Downs <brian.downs@gmail.com>
2024-02-08 15:22:05 -0700

    initil
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- src/vm/datatypes/strings.c
+++ src/vm/datatypes/strings.c
@@ -724,6 +724,60 @@ static Value wrapString(DictuVM *vm, int argCount, Value *args) {
     return OBJ_VAL(takeString(vm, temp, strlen(temp)));
 }
 
+static Value snakeToCamelCaseString(DictuVM *vm, int argCount, Value *args) {
+    if (argCount != 0) {
+        runtimeError(vm, "snakeToCamelCase() takes no arguments (%d given)", argCount);
+        return EMPTY_VAL;
+    }
+
+    ObjString *string = AS_STRING(args[0]);
+    char *temp = ALLOCATE(vm, char, string->length + 1);
+    
+    int i, j;
+    for (i = j = 0; i < string->length; i++, j++) {
+        if (string->chars[i] == '_') {
+            temp[j] = toupper(string->chars[i+1]);
+            i++;
+        } else {
+            temp[j] = string->chars[i];
+        }
+    }
+    temp[j+1] = '\0';
+
+    if (i != j) {
+        temp = SHRINK_ARRAY(vm, temp, char, string->length + 1, strlen(temp) + 1);
+    }
+
+    return OBJ_VAL(takeString(vm, temp, j));
+}
+
+static Value camelToSnakeCaseString(DictuVM *vm, int argCount, Value *args) {
+    if (argCount != 0) {
+        runtimeError(vm, "camelToSanekCase() takes no arguments (%d given)", argCount);
+        return EMPTY_VAL;
+    }
+
+    ObjString *string = AS_STRING(args[0]);
+    char *temp = ALLOCATE(vm, char, string->length + 1);
+    
+    int i, j;
+    for (i = j = 0; i < string->length; i++, j++) {
+        if (isupper(string->chars[i])) {
+            temp[j] = string->chars[i+1];
+            i++;
+        } else {
+            temp[j] = string->chars[i];
+        }
+    }
+    temp[j+1] = '\0';
+
+    if (i != j) {
+        temp = SHRINK_ARRAY(vm, temp, char, string->length + 1, strlen(temp) + 1);
+    }
+
+    return OBJ_VAL(takeString(vm, temp, j));
+}
+
 void declareStringMethods(DictuVM *vm) {
     defineNative(vm, &vm->stringMethods, "len", lenString);
     defineNative(vm, &vm->stringMethods, "toNumber", toNumberString);
@@ -749,4 +803,6 @@ void declareStringMethods(DictuVM *vm) {
     defineNative(vm, &vm->stringMethods, "isLower", isLowerString);
     defineNative(vm, &vm->stringMethods, "collapseSpaces", collapseSpacesString);
     defineNative(vm, &vm->stringMethods, "wrap", wrapString);
+    defineNative(vm, &vm->stringMethods, "snakeToCamelCase", snakeToCamelCaseString);
+    defineNative(vm, &vm->stringMethods, "camelToSnakeCase", camelToSnakeCaseString);
 }
