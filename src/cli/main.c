28cf1baa9909a3191eba87e573d49e19efa9fe8e
Brian Downs <brian.downs@gmail.com>
2024-06-30 21:18:30 -0700

    retrigger CI
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- src/cli/main.c
+++ src/cli/main.c
@@ -95,7 +95,7 @@ char *getDictuPath() {
     } else {
         const char *home = getenv("HOME");
         strncpy(dictuPath, home, strlen(home));
-        strncat(dictuPath, DICTU_HOME, strlen(DICTU_HOME));
+        strncat(dictuPath, DICTU_HOME, strlen(DICTU_HOME)-1);
     }
 
     return dictuPath;
