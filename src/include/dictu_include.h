2202d3a16a06c5da982c97209f68442dd09d4ab6
Liz3 <accs@liz3.net>
2024-11-10 22:37:41 +0100

    feat: add commit hash to version in dictu when compiling

--- src/include/dictu_include.h
+++ src/include/dictu_include.h
@@ -7,7 +7,15 @@
 #define DICTU_MINOR_VERSION "30"
 #define DICTU_PATCH_VERSION "0"
 
+#if defined(GIT_HASH)
+
+#define DICTU_STRING_VERSION "Dictu "  DICTU_MAJOR_VERSION "." DICTU_MINOR_VERSION "." DICTU_PATCH_VERSION "(rev: " GIT_HASH ")\n"
+
+#else
+
 #define DICTU_STRING_VERSION "Dictu Version: " DICTU_MAJOR_VERSION "." DICTU_MINOR_VERSION "." DICTU_PATCH_VERSION "\n"
+
+#endif
 
 typedef struct _vm DictuVM;
 
