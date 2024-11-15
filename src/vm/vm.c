4391dd47a9472b439afb3e8b73d0c8ae2e751814
Liz3 <accs@liz3.net>
2024-11-15 02:24:31 +0100

    fix: remove useless change

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -77,6 +77,7 @@ void runtimeError(DictuVM *vm, const char *format, ...) {
         }
 
         log_pad("");
+
         va_list args;
         va_start(args, format);
         vfprintf(stderr, format, args);
